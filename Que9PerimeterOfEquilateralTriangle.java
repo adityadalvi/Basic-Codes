2c3910356b808fbf2781e4343e3f6316facbaafa
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:21:10 +0530

    Add files via upload

---
+++ Que9PerimeterOfEquilateralTriangle.java
@@ -0,0 +1,20 @@
+public class Que9PerimeterOfEquilateralTriangle
+{
+	public static void main(String[] args) 
+	{
+		int side=10;
+		System.out.println("Perimeter of triangle :"+3*side+" cm*cm");
+		new1(side);
+	}
+	/*
+	public static void new1(int side) 
+	{
+		double perimeter=3*side;
+		System.out.println("Perimeter of triangle :"+perimeter);
+	}*/
+	public static void new1(int side) 
+	{
+		
+		System.out.println("Perimeter of triangle :"+3*side+" cm*cm");
+	}
+}
