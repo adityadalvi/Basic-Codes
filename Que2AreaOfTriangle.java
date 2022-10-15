639cf3cfbc4ff687bec5d8154d144779b0eb1571
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:15:32 +0530

    Add files via upload

---
+++ Que2AreaOfTriangle.java
@@ -0,0 +1,19 @@
+public class Que2AreaOfTriangle
+{
+	public static void main(String[] args)
+	{
+		int base=10;
+		int height=20;
+		System.out.println("Area of Triangle:"+(0.5*base*height)+"cm*cm");
+		myCode(base, height);
+	}
+	/*public static void myCode(int base, int height)
+	{
+		area=0.5*base*height;
+		System.out.println("Area of Triangle:"+area+"cm*cm");
+	}*/
+	public static void myCode(int base, int height) 
+	{
+		System.out.println("Area of Triangle:"+(0.5*base*height)+"cm*cm");
+	}
+}
