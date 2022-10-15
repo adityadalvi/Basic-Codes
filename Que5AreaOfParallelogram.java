fad87e14a7cd1a88867722cea9456ac6403d0157
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:17:10 +0530

    Add files via upload

---
+++ Que5AreaOfParallelogram.java
@@ -0,0 +1,24 @@
+public class Que5AreaOfParallelogram
+{
+	public static void main(String[] args)
+	{
+		int base=100;
+		int height=200;
+		int area=0;
+		System.out.println("Area of parallelogram :"+base*height+"cm*cm");
+		//myCode(base,height,area);
+	}
+	
+	//Type2
+	/*public static void myCode(int base, int height, int area)
+	{
+		area=base*height;
+		System.out.println("Area of parallelogram :"+area+"cm*cm");
+	}
+	//Type3
+	public static void myCode(int base, int height, int r)
+	{
+		System.out.println("Area of parallelogram :"+(r=base*height)+"cm*cm");
+	}*/
+	
+}
