49d51cb6d7235130b6b474a87dfe22723f5b18ef
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:16:46 +0530

    Add files via upload

---
+++ Que4AreaOfIsoscelesTriangle.java
@@ -0,0 +1,28 @@
+public class Que4AreaOfIsoscelesTriangle 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int base=10;
+		int height=20;
+		double area=0;
+		System.out.println("Area of isosceles triangle is: "+0.5*base*height+"cm*cm");
+		//myCode(base, height,area);
+	}
+	
+	//Type2
+	/*public static void myCode(int base, int height, double area)
+	{
+		sarea=0.5*base*height;
+		System.out.println("Area of isosceles triangle is: "+area+"cm*cm");
+	}
+	
+	//Type3
+	public static void myCode(int base, int height, double area)
+	{
+		
+		System.out.println("Area of isosceles triangle is: "+0.5*base*height+"cm*cm");
+	}*/
+
+}
