10c341b148634d9353f7f2e614944b10b7488ba0
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:16:31 +0530

    Add files via upload

---
+++ Que3AreaOfRectangle.java
@@ -0,0 +1,25 @@
+public class Que3AreaOfRectangle
+{
+	public static void main(String[] args) 
+	{
+		int length=10;
+		int breadth=20;
+		int area=0;
+		System.out.println("Area Of Rectangle: "+length*breadth+"cm*cm");
+		//myCode(length, breadth, area);
+	}
+	
+	//Type2
+	/*public static void myCode(int length, int breadth, int area)
+	{
+		area=length*breadth;
+		System.out.println("Area Of Rectangle: "+area+"cm*cm");
+	}
+	
+	//Type3
+	public static void myCode(int length, int breadth, int area)
+	{
+		
+		System.out.println("Area Of Rectangle: "+length*breadth+"cm*cm");
+	}*/
+} 
