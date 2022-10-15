f5c191306370d25712bc8ec29169dd5a2e056591
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:22:41 +0530

    Add files via upload

---
+++ Que13PerimeterOfRhombus.java
@@ -0,0 +1,20 @@
+
+public class Que13PerimeterOfRhombus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int side=10;
+		System.out.println(4*side);
+		//new1(side);
+	}
+	/*public static void new1(int side)
+	{
+		int perimeter=4*side;
+		System.out.println(perimeter);
+	}
+	public static void new1(int side)
+	{
+		System.out.println(4*side);
+	}*/
+
+}
