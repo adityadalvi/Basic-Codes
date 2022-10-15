3ce5a726ddc54798ec8feec5dd457a3f2a04bb81
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:23:42 +0530

    Add files via upload

---
+++ Que17VolumeOfSphere.java
@@ -0,0 +1,24 @@
+public class Que17VolumeOfSphere 
+{
+
+	public static void main(String[] args) 
+	{
+		int rad=10;
+		System.out.println((4*3.142*Math.pow(rad, 3))/3);
+		//new2(rad);
+	}
+	/*
+	public static void new2(int rad)
+	{
+		double volume=(4*3.142*Math.pow(rad, 3))/3;
+		System.out.println(volume);
+	}
+	
+	public static void new2(int rad)
+	{
+		
+		System.out.println((4*3.142*Math.pow(rad, 3))/3);
+	}*/
+
+
+}
