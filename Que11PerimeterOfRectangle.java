ae1ceb55902dd3eba3e731be22338e863a589809
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:22:08 +0530

    Add files via upload

---
+++ Que11PerimeterOfRectangle.java
@@ -0,0 +1,24 @@
+public class Que11PerimeterOfRectangle 
+{
+
+	public static void main(String[] args) 
+	{
+		int l=90000000;
+		int b=90000000;
+		System.out.println(2*(l+b));
+		//new1(l,b);
+	}
+	/*
+	public static void new1(int l, int b) 
+	{
+		int perimeter=2*(l+b);
+		System.out.println(perimeter);
+	}
+	public static void new1(int l, int b) 
+	{
+		
+		System.out.println(2*(l+b));
+	}*/
+
+}
+
