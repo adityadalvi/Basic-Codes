b3a8c274c53df40167c3263fcc4bd980ecb37434
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:22:20 +0530

    Add files via upload

---
+++ Que12PerimeterOfSquare.java
@@ -0,0 +1,22 @@
+
+public class Que12PerimeterOfSquare 
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double side=10.0;
+		System.out.println(4*side);
+		//new1(side);
+	}
+	/*
+	public static void new1(double side) 
+	{
+		double perimeter=4*side;
+		System.out.println(perimeter);
+	}
+	public static void new1(double side) 
+	{
+		System.out.println(4*side);
+	}*/
+
+}
