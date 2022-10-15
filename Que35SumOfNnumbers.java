9db7187ff6a64e3c937f797da71263b5a0984bd1
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:37:32 +0530

    Add files via upload

---
+++ Que35SumOfNnumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Que35SumOfNnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner sc=new Scanner(System.in);
+	int num=sc.nextInt();
+	int sum=0;
+	for (int i = 0; i<=num; i++) {
+		
+		sum=sum+i;
+	}
+	System.out.println(sum);
+
+	}
+
+}
