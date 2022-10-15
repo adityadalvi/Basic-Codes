ae164f27afd33720b49ef315963b3df80f9913dc
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:21:25 +0530

    Add files via upload

---
+++ Que10PerimeterOfParallelogram2.java
@@ -0,0 +1,24 @@
+
+public class Que10PerimeterOfParallelogram2 
+{
+
+	public static void main(String[] args) 
+	{
+		int base=90332;
+		int side=90332;
+		System.out.println(base*side);
+		//new1(side,base);
+	}
+	/*
+	public static void new1(int b, int s)
+	{
+		int area=2*b*s;
+		System.out.println(area);
+	}
+	public static void new1(int b, int s)
+	{
+		
+		System.out.println(2*b*s);
+	}*/
+
+}
