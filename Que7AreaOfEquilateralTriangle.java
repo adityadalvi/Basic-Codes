1dbdb6a00f9ad681cba5919d315b25f81aa69363
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:18:03 +0530

    Add files via upload

---
+++ Que7AreaOfEquilateralTriangle.java
@@ -0,0 +1,25 @@
+
+public class Que7AreaOfEquilateralTriangle 
+{
+
+	public static void main(String[] args) 
+	{
+		int side=10;
+		double area=0;
+		System.out.println("Area of Equilateral Triangle:"+(area=((Math.sqrt(3))*(side*side))/4)+" cm*cm");
+		//myCode(side, area);
+	}
+	/*
+	public static void myCode(int side,double area)
+	{
+		area=((Math.sqrt(3))*(side*side))/4;
+		System.out.println("Area of Equilateral Triangle:"+area+" cm*cm");
+	}
+	
+	public static void myCode(int side,double area)
+	{
+		
+		System.out.println("Area of Equilateral Triangle:"+((Math.sqrt(3))*(side*side))/4+" cm*cm");
+	}*/
+
+}
