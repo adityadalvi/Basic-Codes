00383788fe48e3b1dc1a92a937a6a01a378f4f4f
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:23:14 +0530

    Add files via upload

---
+++ Que15_18VolumeOfPrism.java
@@ -0,0 +1,23 @@
+
+public class Que15_18VolumeOfPrism {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int base=10;
+		int height=20;
+		System.out.println(base*height);
+		//new1(base,height);
+	}
+	
+	/*
+	public static void new1(int base,int height)
+	{
+		int volume=base*height;
+		System.out.println(volume);
+	}
+	public static void new1(int base,int height)
+	{
+		System.out.println(base*height);
+	}*/
+
+}
