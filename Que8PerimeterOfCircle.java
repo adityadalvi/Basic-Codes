15d0cd1b19bfe4dd5535a1e76543e358a3258546
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:20:53 +0530

    Add files via upload

---
+++ Que8PerimeterOfCircle.java
@@ -0,0 +1,23 @@
+public class Que8PerimeterOfCircle 
+{
+	public static void main(String[] args)
+	{
+	    int rad=10;
+	    System.out.println("perimeter: "+2*3.142*rad+" cm*cm");
+	    myCode(rad);
+	}
+	
+	//Type 2
+	/*public static void myCode(int rad)
+	{
+		double perimeter=2*3.142*rad;
+		System.out.println(perimeter);
+	}*/
+	
+	//Type 3
+	public static void myCode(int rad)
+	{
+		System.out.println("perimeter: "+2*3.142*rad+" cm*cm");
+	}
+
+}
