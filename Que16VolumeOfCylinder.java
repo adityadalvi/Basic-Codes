7c8d7232fd5e76044d393df60fc3f957ec008041
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:23:29 +0530

    Add files via upload

---
+++ Que16VolumeOfCylinder.java
@@ -0,0 +1,23 @@
+public class Que16VolumeOfCylinder 
+{
+
+	public static void main(String[] args) 
+	{
+		int rad=10;
+		int height=20;
+		System.out.println(3.142*rad*rad*height);
+		//new1(rad, height);
+	}
+	
+	/*
+	public static void new1(int rad1, int height1)
+	{
+		double volume=3.142*rad1*rad1*height1;
+		System.out.println(volume);
+	}
+	public static void new1(int rad1, int height1)
+	{
+		System.out.println(3.142*rad1*rad1*height1);
+	}
+*/
+}
