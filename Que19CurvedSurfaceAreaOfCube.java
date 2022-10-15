3635eb9ec79e77844b26302ddfb7b19151d82e1e
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:24:01 +0530

    Add files via upload

---
+++ Que19CurvedSurfaceAreaOfCube.java
@@ -0,0 +1,23 @@
+
+public class Que19CurvedSurfaceAreaOfCube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int edge=10;
+		System.out.println(Math.pow(edge, 3));
+		//new1(edge);
+	}
+	
+	/*
+	public static void new1(int edge)
+	{
+		double surfaceArea=Math.pow(edge, 3);
+		System.out.println(surfaceArea);
+	}
+	public static void new1(int edge)
+	{
+		System.out.println(Math.pow(edge, 3));
+	}*/
+	
+
+}
