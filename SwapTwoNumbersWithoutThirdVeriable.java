47fddac4d6d23fe0495aedaa4bf8555039705e68
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:38:45 +0530

    Add files via upload

---
+++ SwapTwoNumbersWithoutThirdVeriable.java
@@ -0,0 +1,16 @@
+
+public class SwapTwoNumbersWithoutThirdVeriable {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a=95;
+		int b=52;
+		a=a+b;   //30
+		b=a-b;   //10
+		a=a-b;   //20
+		System.out.println(a+"\n"+b);
+
+	}
+
+}
