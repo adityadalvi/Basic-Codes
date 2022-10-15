a56f3ffd28d5697476939db2cbfe2c9525b82d38
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:17:32 +0530

    Add files via upload

---
+++ Que6AreaOfRhombus.java
@@ -0,0 +1,28 @@
+
+public class Que6AreaOfRhombus 
+{
+
+	public static void main(String[] args) 
+	{
+		int d1=100; // diagonal 1
+		int d2=100; // diagonal 2
+		double area=0;
+		System.out.println("Area of Rhombus :"+(area=d1*d2/2)+" cm*cm");
+		//myCode(d1,d2,area);
+	}
+	
+	//Type 2
+	/*public static void myCode(int d1, int d2,double area)
+	{
+		area=d1*d2/2;
+		System.out.println("Area of Rhombus :"+area+" cm*cm");
+	}
+	
+	//Type3
+	public static void myCode(int d1, int d2,double area)
+	{
+		
+		System.out.println("Area of Rhombus :"+d1*d2/2+" cm*cm");
+	}*/
+
+}
