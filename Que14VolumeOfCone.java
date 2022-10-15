043edef2eeff3b9a24465de3f41eefe83be6779c
Aditya Dalvi <96537068+adityadalvi@users.noreply.github.com>
2022-10-15 17:22:57 +0530

    Add files via upload

---
+++ Que14VolumeOfCone.java
@@ -0,0 +1,24 @@
+public class Que14VolumeOfCone 
+{
+
+	public static void main(String[] args) 
+	{
+		int rad=10;
+		double height=50;
+		System.out.println(3.142*rad*rad*(height/3));
+		//new1(rad,height);
+	}
+	
+	/*
+	public static void new1(int rad, double height)
+	{
+		
+		double volume=3.142*rad*rad*(height/3);
+		System.out.println(volume);
+	}
+	public static void new1(int rad, double height)
+	{
+		System.out.println(3.142*rad*rad*(height/3));
+	}*/
+
+}
